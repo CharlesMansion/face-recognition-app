8dd72c3ec3b7927cddff9e3f36c1ed53b7e7ec7d
Nicolas Nollomont <nicolasnollomont@whatever.piascom.global>
2018-05-09 10:05:50 +0200

    adding files

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import './app.css';
 import Rank from './Components/Rank/Rank'
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+                particles: {
+                  number: {
+                    value:60,
+                    density: {
+                      enable:true,
+                      value_area:800
+                    }
+                  }
+                }
+              }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+       <Particles className='particles' 
+              params={particlesOptions}
+         />
       <Navigation />
       <Logo />
       <Rank />
