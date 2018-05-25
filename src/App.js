3726425b3e5bdb47388a6781d111e463688fb4c6
Nicolas Nollomont <nicolasnollomont@whatever.piascom.global>
2018-05-25 16:45:46 +0200

    adding files

--- src/App.js
+++ src/App.js
@@ -7,12 +7,8 @@ import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import './app.css';
 import Rank from './Components/Rank/Rank'
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 
-const app = new Clarifai.App({
- apiKey: 'ba14731d87a44d498fba56b4434db4d1'
-});
 
 const particlesOptions = {
                 particles: {
@@ -81,10 +77,14 @@ onInputChange = (event) => {
 
 onButtonSubmit = () => {
   this.setState({imageUrl: this.state.input});
-  app.models
-  .predict(
-    Clarifai.FACE_DETECT_MODEL,
-    this.state.input)
+  fetch('http://localhost:3001/imageurl', {
+      method:'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+  .then(response => response.json())
   .then(response => {
     if (response) {
     fetch('http://localhost:3001/image', {
